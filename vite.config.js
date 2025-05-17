dcc5dbad74cb96697992e0506392150785f15b74
Diego Gualdron <dgualdron04@gmail.com>
2025-05-17 18:40:45 -0500

    Change name of base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedex-vue/',
+  base: '/Pokedex/',
   plugins: [vue()],
   resolve: {
     alias: {
